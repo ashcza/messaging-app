f39e4ca91178946e9d7c252068bf4e0bb88a196a
ashcza <ashcon.zand@gmail.com>
2017-04-24 19:16:09 -0400

    create SendMessage component

---
+++ frontend/components/SendMessage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class SendMessage extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      message: ""
+    }
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(e) => {
+    this.setState({message: e.target.value});
+  }
+
+  handleSubmit(e) => {
+    e.preventDefault();
+    this.props.sendMessage(this.state.message);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.message} onChange={this.update} />
+        </label>
+        <input type="submit" value="Enter Message" />
+      </form>
+    )
+  }
+}
